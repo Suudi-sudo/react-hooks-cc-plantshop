93b00ea398a759553c390bcb8a058e315cb53bca
suudi abdisalan <abdisalansucdi@gmail.com>
2024-11-10 19:19:55 +0300

    complete challenge

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,15 +1,22 @@
+
+	
 import React from "react";
 
-function PlantCard() {
+function PlantCard({plant, onUpdatePlant}) {
+
+  const handleSoldOutClick = () => {
+    const updatedPlant = {...plant, soldOut: true};
+    onUpdatePlant(updatedPlant);
+  }
   return (
     <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      <img src={plant.image || " http://localhost:6001/plants"} alt={plant.name || "Plant"} />
+      <h4>{plant.name || "Unnamed Plant"}</h4>
+      <p>Price: {plant.price || "Unknown"}</p>
+      {plant.soldOut ? (
+        <button className="sold-out">Out of Stock</button>
       ) : (
-        <button>Out of Stock</button>
+        <button className="primary" onClick={handleSoldOutClick}>In Stock</button>
       )}
     </li>
   );
